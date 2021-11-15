5636264efa8f7f98dc712a3ba9122baecfdb2b7f
Marten Oosterhoff <marten@oosterhoff.de>
2021-11-15 07:55:06 +0100

    Set proper metadata for customized system.

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemX8664.MixProject do
+defmodule NervesInfluxDB2X8664.MixProject do
   use Mix.Project
 
-  @github_organization "nerves-project"
-  @app :nerves_system_x86_64
+  @github_organization "ooodigi"
+  @app :nerves_influxdb2_x86_64
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
